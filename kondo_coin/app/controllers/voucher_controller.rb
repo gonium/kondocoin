964d2c1b269de8c37c8f2a5fa591cb6eda273eba
Mathias Dalheimer <md@gonium.net>
2013-08-13 20:51:28 +0200

    styled voucher index, accepting correct codes, rejecting invalid codes.

--- kondo_coin/app/controllers/voucher_controller.rb
+++ kondo_coin/app/controllers/voucher_controller.rb
@@ -1,6 +1,7 @@
 class VoucherController < ApplicationController
 
-  attr_accessor :btc_address, :code1, :code2, :code3, :code4
+  attr_accessor :btc_address, 
+    :code1, :code2, :code3, :code4, :code5, :code6
 
   # claiming the bitcoins in here!
   def index
@@ -9,7 +10,21 @@ class VoucherController < ApplicationController
 
   def redeem
     # TODO: Compose voucher code, search in DB.
-    #@voucher = Voucher.find(params[:id]) || Voucher.new
+    code = "#{params[:voucher][:code1]}-"
+    code += "#{params[:voucher][:code2]}-"
+    code += "#{params[:voucher][:code3]}-"
+    code += "#{params[:voucher][:code4]}-"
+    code += "#{params[:voucher][:code5]}-"
+    code += "#{params[:voucher][:code6]}"
+    voucher = Voucher.find_by_code(code);
+    if voucher && params[:voucher][:btc_address]
+      # Attempt to create payout 
+      #flash[:success] = 'You ' 
+      render 'redeem'
+    else
+      flash[:error] = 'Invalid code.'
+      render 'index'
+    end
   end
 
 end
--- kondo_coin/spec/requests/voucher_pages_spec.rb
+++ kondo_coin/spec/requests/voucher_pages_spec.rb
@@ -7,10 +7,10 @@ describe "VoucherPages" do
   end
 
   let(:basetitle) { "Kondoco.in" }
+  let(:btc_payout_address) { "fjiwerjiowejfiwjefijweio" }
   subject { page }
 
   describe "redeem voucher" do
-    let (:voucher) { @v_factory.create(1) }
     before { visit voucher_index_path }
 
     describe "page" do
@@ -20,7 +20,24 @@ describe "VoucherPages" do
 
     describe "with invalid information" do
       before {click_button "Claim now" }
-      it { should have_content('error') }
+      it { should have_content('Invalid code') }
+    end
+
+    describe "with correct information" do
+      before do
+        @v_factory.create(1)
+        voucher = Voucher.last
+        codesections = voucher.code.split("-");
+        fill_in "voucher_code1", with: codesections[0];
+        fill_in "voucher_code2", with: codesections[1];
+        fill_in "voucher_code3", with: codesections[2];
+        fill_in "voucher_code4", with: codesections[3];
+        fill_in "voucher_code5", with: codesections[4];
+        fill_in "voucher_code6", with: codesections[5];
+        fill_in "voucher_btc_address", with: :btc_payout_address;
+        click_button "Claim now" 
+      end
+      it { should have_content('Success!') }
     end
   end
 
