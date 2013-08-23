6a950333d594747617ea87ad9a2f17cfe96cb879
Mathias Dalheimer <md@gonium.net>
2013-08-24 01:12:53 +0200

    added security foo, added simplecov reporting

--- kondo_coin/config/initializers/filter_parameter_logging.rb
+++ kondo_coin/config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :code1, :code2, :code3, :code4, :code5, :code6]
