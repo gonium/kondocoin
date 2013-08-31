393cfa108523528c0b518b08c162a7b8e5a82bf2
Mathias Dalheimer <md@gonium.net>
2013-08-31 20:47:49 +0200

    language selection fully functional.

--- kondo_coin/app/controllers/application_controller.rb
+++ kondo_coin/app/controllers/application_controller.rb
@@ -2,5 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_locale
 
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
+  end
 end
