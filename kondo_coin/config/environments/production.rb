716b0934baf602275b074541b99c9e03b01f2a24
Mathias Dalheimer <md@gonium.net>
2014-01-04 22:35:44 +0100

    added video of 30c3

--- kondo_coin/config/environments/production.rb
+++ kondo_coin/config/environments/production.rb
@@ -59,7 +59,7 @@ KondoCoin::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
